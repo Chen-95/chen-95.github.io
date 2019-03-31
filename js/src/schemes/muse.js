d558c42e042af6226b99b42a1e923c4fdaea2b54
Chen-95 <clh950818@163.com>
2019-03-31 21:27:18 +0800

    Site updated: 2019-03-31 21:26:58

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sun Mar 24 2019 13:12:13 GMT+0800 (GMT+08:00)
+// build time:Sun Mar 31 2019 21:26:41 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
